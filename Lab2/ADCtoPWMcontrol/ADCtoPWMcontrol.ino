1b2a457f917cad7ddf26acf0e2bc7a62a0e27526
tlechman49 <tlechman49@gmail.com>
2019-04-16 12:41:30 -0400

    Changes during lab 2

--- Lab2/ADCtoPWMcontrol/ADCtoPWMcontrol.ino
+++ Lab2/ADCtoPWMcontrol/ADCtoPWMcontrol.ino
@@ -4,9 +4,13 @@ int pin_out=9;
 void setup() {
   // put your setup code here, to run once:
   TCCR2B = (TCCR2B & 0xF8) | 0x03;
+
+  Serial.begin(9600);
+  Serial.println("Hello World");
 }
 void loop() {
   // put your main code here, to run repeatedly:
   val=analogRead(pin_in);
-  analogWrite(pin_out,val);
+  Serial.println(val);
+  analogWrite(pin_out,val/4);
 }
