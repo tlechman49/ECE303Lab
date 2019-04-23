f601c2e6787e26c87f6d5099255103d3cf6a9d03
mkowsiany <mkowsiany@gmail.com>
2019-04-22 20:43:17 -0400

    Create PreLab3_CurrentVoltage.m

---
+++ Lab3/PreLab3_CurrentVoltage.m
@@ -0,0 +1,26 @@
+close all;clear all;clc
+
+% Create equipment connections and objects
+DMM=visa('agilent', 'USB0::0x2A8D::0xB318::MY58160068::0::INSTR');
+
+% Open instruments
+fopen(DMM);
+
+fprintf(awg,'OUTP:LOAD INF'); % Place waveform generator into high-Z
+
+% Conduct experiment by sweeping waveform generator voltage
+Imeas = [];
+Vmeas = [];
+
+while Vmeas(-1) > 4.8
+    pause(1)
+    voltage = str2num(query(DMM, 'MEAS:VOLT:DC?'));
+    current = str2num(query(DMM, 'MEAS:CURR:DC?'));
+    Vmeas = [Vmeas ; voltage];
+    Imeas = [Imeas ; -current];
+end
+
+% Close and delete instrument connections and objects
+fclose(DMM);
+delete(DMM);
+
