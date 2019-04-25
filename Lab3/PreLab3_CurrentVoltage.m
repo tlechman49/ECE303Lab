e9032f35e544d322faba4724321ac3d790427f23
Timothy Lechman <31293866+tlechman49@users.noreply.github.com>
2019-04-25 19:05:47 -0400

    Battery drainage data and uploaded .m file

--- Lab3/PreLab3_CurrentVoltage.m
+++ Lab3/PreLab3_CurrentVoltage.m
@@ -1,24 +1,22 @@
 close all;clear all;clc
 
 % Create equipment connections and objects
-DMM=visa('agilent', 'USB0::0x2A8D::0xB318::MY58160068::0::INSTR');
+DMM=visa('agilent', 'USB0::0x2A8D::0xB318::MY58150015::0::INSTR');
 
-% Open instruments
+% Open instruments 
 fopen(DMM);
 
-fprintf(awg,'OUTP:LOAD INF'); % Place waveform generator into high-Z
-
 % Conduct experiment by sweeping waveform generator voltage
-Imeas = [];
-Vmeas = [];
-t = [];
+Imeas = [0];
+Vmeas = [5];
+t = [0];
 tic();
-while Vmeas(-1) > 4.8
-    pause(1)
+while Vmeas(end) > 4.8
+    %pause(1)
     voltage = str2num(query(DMM, 'MEAS:VOLT:DC?'));
-    current = str2num(query(DMM, 'MEAS:CURR:DC?'));
-    Vmeas = [Vmeas ; voltage];
-    Imeas = [Imeas ; -current];
+    current = str2num(query(DMM, 'MEAS:CURR:DC? 1, 3.0e-5'));
+    Vmeas = [Vmeas ; -voltage];
+    Imeas = [Imeas ; current];
     t = [t ; toc()];
 end
 
